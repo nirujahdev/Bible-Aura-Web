c30f479539b97f9ec3b61994fd7a828b651effd8
nirujahdev <niru67@gmail.com>
2025-11-18 20:38:50 +0530

    Fix closing parenthesis in mobileNavItems map

--- src/components/ModernSidebar.tsx
+++ src/components/ModernSidebar.tsx
@@ -389,7 +389,8 @@ function MobileNavigationHeader() {
                         </div>
                       </div>
                     </Link>
-                  ))}
+                    );
+                  })}
                 </div>
               </div>
 
